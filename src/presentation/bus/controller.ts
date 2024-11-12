4402a705dbd92ee00d02f2534e47349129455dd3
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2024-11-11 23:49:30 -0500

    quitando apis innecesarias, llamando la el servicio de recibir datos del websocket

--- src/presentation/bus/controller.ts
+++ src/presentation/bus/controller.ts
@@ -5,17 +5,5 @@ export class BusController {
     constructor(
         private readonly busService = new BusService()
     ) { }
-<<<<<<< HEAD
-
-
-    public getBusCoordinate = async (req: Request, res: Response) => {
-        res.json(this.busService.buses);
-    }
-
-    public sendBusCoordinate = async (req: Request, res: Response) => {
-        res.json(this.busService.createBusCoordinate);
-    }
-=======
->>>>>>> aca161d (quitando apis innecesarias, llamando la el servicio de recibir datos del websocket)
     
 }
